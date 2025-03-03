c8011fd5183c17f18c5b2b4e7bb7b26209ae1673
YelyzavetaAf <yelyzaveta.afanasenko@g.austincc.edu>
2025-03-03 15:43:39 +0000

    Updated decisions.py

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,16 @@
-#
+def get_options_ratio(option,total):
+    if total == 0:
+        raise ValueError("Total cannot be zero.")
+    return option / total
+
+def get_faculty_rating(ratio):
+    if ratio >= 0.9 and ratio < 1:
+        return "Excellent"
+    elif ratio > 0.8:
+        return "Very Good"
+    elif ratio > 0.7:
+        return "Good"
+    elif ratio > 0.6:
+        return "Needs Improvement"
+    else:
+        return "Unacceptable"
