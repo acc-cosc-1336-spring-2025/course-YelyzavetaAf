5f184d19393da8f97ab4e46c84022bbe6acafed8
YelyzavetaAf <yelyzaveta.afanasenko@g.austincc.edu>
2025-03-03 17:19:55 +0000

    Completed homework assignment

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,18 @@
-#
+from src.homework.c_decisions.decisions import get_options_ratio, get_faculty_rating
+
+def main():
+    option = int(input("Enter the number of options: "))
+    total = int(input("Enter the total number: "))
+
+    try:
+        ratio = get_options_ratio(option, total)
+        rating = get_faculty_rating(ratio)
+
+        print(f"Ratio: {ratio:.2f}")
+        print(f"Faculty Rating: {rating}")
+
+    except ValueError as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -1,1 +1,28 @@
-#
+import unittest
+from src.homework.c_decisions.decisions import get_options_ratio, get_faculty_rating
+
+class TestFacultyRating(unittest.TestCase):
+
+    def test_get_options_ratio(self):
+        self.assertEqual(get_options_ratio(5, 20), 0.25)
+        self.assertEqual(get_options_ratio(10, 20), 0.5)
+        self.assertEqual(get_options_ratio(7, 10), 0.7)
+        self.assertEqual(get_options_ratio(6, 10), 0.6)
+        with self.assertRaises(ValueError):
+            get_options_ratio(5, 0)
+
+def test_get_faculty_rating(self):
+    self.assertEqual(get_faculty_rating(0.91), "Excellent")
+    self.assertEqual(get_faculty_rating(0.85), "Very Good")
+    self.assertEqual(get_faculty_rating(0.71), "Good")
+    self.assertEqual(get_faculty_rating(0.66), "Needs Improvement")
+    self.assertEqual(get_faculty_rating(0.45), "Unacceptable")
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+    
+   
+    
+                
