eb5c6bbc976744ac6e48ea5afd81deb5d1f4ab0c
YelyzavetaAf <yelyzaveta.afanasenko@g.austincc.edu>
2025-03-08 19:12:17 +0000

    Homework 2

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,10 +1,15 @@
-from src.homework.c_decisions.decisions import get_options_ratio, get_faculty_rating
+from decisions import get_options_ratio, get_faculty_rating
 
 def main():
-    option = int(input("Enter the number of options: "))
-    total = int(input("Enter the total number: "))
-
     try:
+
+        option = int(input("Enter the number of options: "))
+        total = float(input("Enter the total number: "))
+        if total == 0:
+            print("Error: Total number cannot be zero.")
+            return
+
+    
         ratio = get_options_ratio(option, total)
         rating = get_faculty_rating(ratio)
 
@@ -12,7 +17,7 @@ def main():
         print(f"Faculty Rating: {rating}")
 
     except ValueError as e:
-        print(f"Error: {e}")
+        print(f"Error: Invalid input! {e}")
 
 if __name__ == "__main__":
     main()
