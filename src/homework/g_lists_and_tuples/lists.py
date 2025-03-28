615e056e00fc9fc124ba3229f8eda197e9e390e2
YelyzavetaAf <yelyzaveta.afanasenko@g.austincc.edu>
2025-03-28 15:13:08 +0000

    Homework 5

--- src/homework/g_lists_and_tuples/lists.py
+++ src/homework/g_lists_and_tuples/lists.py
@@ -1,1 +1,14 @@
-#
+def get_lowest_list_value(lst):
+    lowest = lst[0]
+    for num in lst:
+        if num < lowest:
+            lowest = num
+    return lowest
+
+
+def get_highest_list_value(lst):
+    highest = lst[0]
+    for num in lst:
+        if num > highest:
+            highest = num
+    return highest
--- src/homework/g_lists_and_tuples/main.py
+++ src/homework/g_lists_and_tuples/main.py
@@ -1,1 +1,34 @@
-#
+from lists import get_lowest_list_value, get_highest_list_value
+
+def main():
+    while True:
+        print("\n1 - Show the list low/high values")
+        print("2 - Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == "2":
+            print("Exiting program...")
+            break
+        elif choice == "1":
+            numbers = []
+            while True:
+                try:
+                    value = int(input("Enter a list value: "))
+                    numbers.append(value)
+                except ValueError:
+                    print("Please enter a valid number.")
+                    continue
+
+                if len(numbers) >= 3:
+                    more = input("Do you want to enter another value? (y/n): ")
+                    if more.lower() != 'y':
+                        break
+
+            print("Lowest value:", get_lowest_list_value(numbers))
+            print("Highest value:", get_highest_list_value(numbers))
+        else:
+            print("Invalid choice. Please try again.")
+if __name__ == "__main__":
+    main()
+
+        
